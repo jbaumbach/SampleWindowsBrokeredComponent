a4064374e6ecbb59ffd0d2150abc243a67da353a
John Baumbach <john.j.baumbach@gmail.com>
2016-07-18 10:41:51 -0700

    Added code to instantiate C# object

--- SampleBrokeredThingy/MainPage.xaml.cs
+++ SampleBrokeredThingy/MainPage.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BrokeredComponent;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -29,8 +17,8 @@ namespace SampleBrokeredThingy
 
             Debug.WriteLine("yeah bro, launched and junk");
 
-            //Sample b = new Sample();
-            //Debug.WriteLine(b.Yo());
+            Sample b = new Sample();
+            Debug.WriteLine(b.Yo());
 
         }
     }
